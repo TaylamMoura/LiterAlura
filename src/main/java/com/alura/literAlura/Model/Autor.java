deda11b5b0ebbaabf004ed8c528dff278244ddf4
Taylam Moura <tay.moura@hotmail.com>
2024-12-04 19:59:16 -0300

    adicionado as marcações para banco de dados

--- src/main/java/com/alura/literAlura/Model/Autor.java
+++ src/main/java/com/alura/literAlura/Model/Autor.java
@@ -2,17 +2,43 @@ package com.alura.literAlura.Model;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
 
+import java.util.List;
+
+@Entity
+@Table(name = "autores")
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Autor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     @JsonAlias("name")
+    @Column(name = "nome")
     private String nome;
 
     @JsonAlias("birth_year")
+    @Column(name = "nascimento")
     private int nascimento;
 
     @JsonAlias("death_year")
+    @Column(name = "ano_falecimento")
     private int anoFalecimento;
+
+    @OneToMany(mappedBy = "autor", cascade = CascadeType.ALL)
+    private List<LivroAutor> livroAutores;
+
+    public Autor(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getNome() {
         return nome;
@@ -36,6 +62,14 @@ public class Autor {
 
     public void setAnoFalecimento(int anoFalecimento) {
         this.anoFalecimento = anoFalecimento;
+    }
+
+    public List<LivroAutor> getLivroAutores() {
+        return livroAutores;
+    }
+
+    public void setLivroAutores(List<LivroAutor> livroAutores) {
+        this.livroAutores = livroAutores;
     }
 
     @Override
