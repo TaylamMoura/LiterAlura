daab11d3bb951f38d26b2b3eb33b1c4bee9b233f
Taylam Moura <tay.moura@hotmail.com>
2024-12-16 15:36:22 -0300

    Criação das querys: autoresCadastrados, autoresVivosNoAno e livrosPorIdioma.

--- src/main/java/com/alura/literAlura/Repository/AutorRepository.java
+++ src/main/java/com/alura/literAlura/Repository/AutorRepository.java
@@ -1,7 +1,22 @@
 package com.alura.literAlura.Repository;
 
 import com.alura.literAlura.Model.Autor;
+import com.alura.literAlura.Model.Livro;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface AutorRepository extends JpaRepository<Autor, Long> {
+
+    @Query("SELECT a FROM Autor a")
+    List<Autor> autoresCadastrados();
+
+    @Query("SELECT a FROM Autor a WHERE a.nascimento <= :ano AND (a.anoFalecimento >= :ano OR a.anoFalecimento = 0)")
+    List<Autor> autoresVivosNoAno(@Param("ano") int ano);
+
+    @Query("SELECT a FROM Livro a WHERE :livroIdioma MEMBER OF a.idioma")
+    List<Livro> livrosPorIdioma(String livroIdioma);
+
 }
